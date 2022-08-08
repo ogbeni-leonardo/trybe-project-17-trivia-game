c13e85985b8ef79ac00f95b3ec348d13a27adf95
Thiago Viana <trodrigo734@gmail.com>
2022-08-08 17:16:43 -0300

    Resolve o requisito 16

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 class Content extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class Content extends React.Component {
         <Route exact path="/game" component={ Game } />
         <Route path="/settings" component={ Settings } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     );
   }
