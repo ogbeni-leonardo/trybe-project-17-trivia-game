3125438070b150c4df9f6cd227b08690ec23d350
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-08-11 18:15:51 -0300

    concluindo alteracoes

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { number, shape, string } from 'prop-types';
 import { Redirect } from 'react-router-dom';
-
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
